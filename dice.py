9cefee26723d2c8333adbad3a8624b5741683f6f
abhimadd <43162639+abhimadd@users.noreply.github.com>
2018-09-24 14:04:15 +0530

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,2 +1,12 @@
 import random
 print(random.randint(1,6))
+
+output
+1
+cl315@soetcse:~/abhishek$ python3 dice.py
+4
+cl315@soetcse:~/abhishek$ python3 dice.py
+6
+cl315@soetcse:~/abhishek$ python3 dice.py
+6
+cl315@soetcse:~/abhishek$ 
