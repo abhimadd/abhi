96ab656ac406d3fe70887618fd3ee6f66728b3b6
abhimadd <43162639+abhimadd@users.noreply.github.com>
2018-09-24 14:01:13 +0530

    Update lessthan.py

--- lessthan.py
+++ lessthan.py
@@ -10,3 +10,10 @@ if(a<b):
 	print(a,"is lesser")
 else:
 	print(b,"is lesser")
+	output
+	cl315@soetcse:~/abhishek$ python3 lessthan.py
+enter value of A:10
+enter value of B:5
+A: 10
+B: 5
+5 is lesser
