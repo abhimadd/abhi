32c39bd3ae10ee2f54118f8a2bda4e92793406a6
abhimadd <43162639+abhimadd@users.noreply.github.com>
2018-09-24 13:59:10 +0530

    Update greaterthan.py

--- greaterthan.py
+++ greaterthan.py
@@ -12,3 +12,13 @@ if(a>b):
 	print(a,"is greater")
 else:
 	print(b,"is greater")
+
+	output
+	cl315@soetcse:~/abhishek$ python3 greaterthan.py
+enter value of A:12
+enter value of B:10
+A: 12
+B 10
+12 is greater
+cl315@soetcse:~/abhishek$ 
+
