ff06a94d95f2395daccf2cf44c3f8ddea30aa6c9
abhimadd <43162639+abhimadd@users.noreply.github.com>
2018-09-24 13:54:56 +0530

    Update mathopt.py

--- mathopt.py
+++ mathopt.py
@@ -13,3 +13,17 @@ f=a/b
 print("division of two numbers",f)
 g=a%b
 print("percentage of two numbers",g)
+
+output
+cl315@soetcse:~/abhishek$ python3 mathop.py
+python3: can't open file 'mathop.py': [Errno 2] No such file or directory
+cl315@soetcse:~/abhishek$ python3 mathopt.py
+enter a value10
+enter b value6 
+addition of two numbers 16
+subtraction of two numbers 4
+multiplication of two numbers 60
+division of two numbers 1.6666666666666667
+percentage of two numbers 4
+cl315@soetcse:~/abhishek$ 
+
